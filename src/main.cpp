ccfc16a19ae3668132f16370e02487e9e18cddcd
Arno Proeme <a.proeme@epcc.ed.ac.uk>
2021-05-20 01:28:54 +0100

    Forgot to uncomment custom typemap initialisation in main after debugging

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
 
-    //LibGeoDecomp::Typemaps::initializeMaps(); // initialize LibGeoDecomp default typemaps (this commits MPI types)
-    //Typemaps::initializeMaps(); // initialize custom typemaps for HAIL-CAESAR
+    LibGeoDecomp::Typemaps::initializeMaps(); // initialize LibGeoDecomp default typemaps (this commits MPI types)
+    Typemaps::initializeMaps(); // initialize custom typemaps for HAIL-CAESAR
     LibGeoDecomp::MPILayer().barrier();
     
 #ifndef GIT_REVISION
